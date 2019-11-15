5a528c2d74562eb41267878e19faad06c23c6a11
Max Desiatov <max@desiatov.com>
2019-11-15 10:29:06 +0000

    Use GitHub Actions upload/download steps

--- prepare-package.sh
+++ prepare-package.sh
@@ -2,9 +2,5 @@
 
 cd ..
 git clone https://github.com/swiftwasm/swiftwasm-package-sdk.git
-mkdir swiftwasm-package-sdk/prebuilt
-cd swiftwasm-package-sdk/prebuilt
-wget http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-darwin-apple.tar.xz
-cp ../../icu.tar.xz icu4c-wasi.tar.xz
-cp ../../wasi-sdk.tar.gz wasi-sdk-prebuilt-linux.tar.gz
-cd ../../swift
+cd swiftwasm-package-sdk
+./download-installable-prebuilts.sh
