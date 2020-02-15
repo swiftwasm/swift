fdf21bf39bbe0f28111f16ae8aa1608bd2d2c5a2
Max Desiatov <max@desiatov.com>
2020-02-15 21:04:13 +0000

    Update install-dependencies.sh

--- utils/webassembly/linux/install-dependencies.sh
+++ utils/webassembly/linux/install-dependencies.sh
@@ -9,7 +9,7 @@ sudo apt install -y \
   libedit-dev libxml2-dev libsqlite3-dev swig \
   libpython-dev libncurses5-dev pkg-config \
   libblocksruntime-dev libcurl4-openssl-dev \
-  systemtap-sdt-dev tzdata rsync wget llvm
+  systemtap-sdt-dev tzdata rsync wget llvm zip unzip
 
 SOURCE_PATH="$( cd "$(dirname $0)/../../../.." && pwd )" 
 SWIFT_PATH=$SOURCE_PATH/swift
