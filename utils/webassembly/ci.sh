52988ad78270f48c46069b1c5ba71b4b651457a2
Yuta Saito <kateinoigakukun@gmail.com>
2023-01-02 04:26:57 +0000

    Update update-checkout-config.json for wasm/5.8

--- utils/webassembly/ci.sh
+++ utils/webassembly/ci.sh
@@ -6,7 +6,7 @@ SOURCE_PATH="$(cd "$(dirname $0)/../../.." && pwd)"
 SWIFT_PATH=$SOURCE_PATH/swift
 UTILS_PATH=$SWIFT_PATH/utils/webassembly
 
-$SWIFT_PATH/utils/update-checkout --clone --scheme wasm --skip-repository swift
+$SWIFT_PATH/utils/update-checkout --clone --scheme wasm/5.8 --skip-repository swift
 $UTILS_PATH/install-build-sdk.sh
 
 export SCCACHE_CACHE_SIZE="50G"
