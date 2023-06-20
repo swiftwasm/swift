1817305a351be3dfad24814d9226826895339f61
Yuta Saito <kateinoigakukun@gmail.com>
2023-06-20 09:51:29 +0000

    [wasm][build] HACK: Add SwiftWasm compiler vendor support
    This patch should not be upstreamed. Maybe we can use
    `--compiler-vendor=none` to avoid hardcoding specific downstream names.

--- utils/swift_build_support/swift_build_support/products/llvm.py
+++ utils/swift_build_support/swift_build_support/products/llvm.py
@@ -64,6 +64,13 @@ class LLVM(cmake_product.CMakeProduct):
         if self.args.compiler_vendor == "none":
             return []
 
+        if self.args.compiler_vendor == "swiftwasm":
+            return [
+                ('CLANG_VENDOR', 'SwiftWasm'),
+                ('CLANG_VENDOR_UTI', 'org.swiftwasm.compilers.llvm.clang'),
+                ('PACKAGE_VERSION', str(self.args.clang_user_visible_version))
+            ]
+
         if self.args.compiler_vendor != "apple":
             raise RuntimeError("Unknown compiler vendor?!")
 
