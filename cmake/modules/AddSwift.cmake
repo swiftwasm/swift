6cb9e612fa6c8531f46d2c18f92b560a710baf61
Yuta Saito <kateinoigakukun@gmail.com>
2023-06-20 07:04:03 +0000

    [wasm][cmake] HACK: link wasi-emulated-mman to host executable
    The reason why this change is needed is unclear to me...

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -397,6 +397,8 @@ function(_add_host_variant_link_flags target)
       cxx_link_libraries)
     target_link_libraries(${target} PRIVATE
       ${cxx_link_libraries})
+  elseif("${LFLAGS_SDK}" STREQUAL "WASI")
+    list(APPEND result "-Wl,wasi-emulated-mman")
   else()
     # If lto is enabled, we need to add the object path flag so that the LTO code
     # generator leaves the intermediate object file in a place where it will not
